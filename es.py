a473e97d808a31fd364ede36fe71282cd4052ec0
lorenzo <l.baggio@isitgo.it>
2016-06-30 08:39:47 +0000

    Ottimizzazione2 di es

--- es.py
+++ es.py
@@ -15,12 +15,15 @@ temp=np.array(t)#creo array
 #print(temp)
 media=[]#lista vuota
 #print(temp.shape)
-n_fov=temp.shape[0]#1000 temperature per ogni livello
-n_al=temp.shape[1]#61 livelli
-somma=0
-for i in range(n_fov):#fino  1000 
-    somma+=temp[i]
-media.append(somma/n_fov)
+#n_fov=temp.shape[0]#1000 temperature per ogni livello
+#n_al=temp.shape[1]#61 livelli
+#somma=0
+#for i in range(n_fov):#fino  1000 
+   # somma+=temp[i]
+#media.append(somma/n_fov)
+#print(media)
+#somma=np.sum(temp,axis=0) somma senza il for
+media=np.mean(temp,axis=0) 
 print(media)
 
 fg.close()
